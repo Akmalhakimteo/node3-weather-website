079080dcb744a5985f1f41b3fd9bc2a35c8ff43a
Akmal Hakim Teo <akmal_hakim_teo@hotmail.com>
2020-08-21 22:25:38 +0800

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
-
+const port = process.env.PORT || 3000;
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
+
 
 console.log(__dirname);
 console.log(path.join(__dirname, "../public"));
@@ -95,6 +96,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.listen(port, () => {
+  console.log("Listening on port ",port);
 });
